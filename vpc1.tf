8d62543cc60448cebefda2279a12c30f5b5bb4d3
Rani <ranicse338@gmail.com>
2022-08-13 16:38:24 +0530

    c3

--- vpc1.tf
+++ vpc1.tf
@@ -1,15 +1,19 @@
 variable "aws_vpc_cidr"{
 }
+/* changes done by seshi*/
+ variable "aws_sn_cidr"{
+   type=list
+}
+  variable "aws_azs"{
+     type=list
+}
+
   resource "aws_vpc" "vpc1{
    cidr_block=var.v_vpc_cidr
 }
    resource "aws_subnet" "SN1"{
-    vpc_id=aws_vpc_cidr.vpc1.id
-    cidr_block="10.0.0.0/24"
-    availability_zone="ap_southeast_1a"
-}
-resource "aws_subnet" "SN2"{
-    vpc_id=aws_vpc_cidr.vpc1.id
-    cidr_block="10.0.0.0/24"
-    availability_zone="ap_southeast_1a"
+     count=length(var.v_sn_cidr)
+     vpc_id=aws_vpc_cidr.vpc1.id
+      cidr_block=var.v_sn_cidr[count.index]
+     availability_zone=var.v_azs[count.index]
 }
